55511965e3f1abb98a7e41fa3f6581bf0e691d5c
adriengvd <adrien.gervraud@gmail.com>
2023-01-16 23:30:42 +0100

    use swagger

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -55,11 +55,11 @@ static WebApplication MigrateDatabase(WebApplication app)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+app.UseSwagger();
+app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
